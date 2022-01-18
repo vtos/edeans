48b1a8e76befd562af093605b9fe8a8e60a9c63b
Vitaly Potenko <potenkov@gmail.com>
2022-01-18 12:34:53 +0300

    Added basic contract for the form of control repository.

---
+++ src/Domain/Model/FormOfControl/FormOfControlRepository.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Edeans\Domain\Model\FormOfControl;
+
+interface FormOfControlRepository
+{
+    public function getOneById(FormOfControlId $id): FormOfControl;
+
+    public function findByName(FormOfControlName $name): ?FormOfControl;
+
+    public function store(FormOfControl $formOfControl): void;
+
+    public function remove(FormOfControl $formOfControl): void;
+}
