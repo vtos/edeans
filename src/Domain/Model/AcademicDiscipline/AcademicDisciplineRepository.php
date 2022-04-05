5d3d3521b972c8831f24a9a8af4cede11ce7fefa
vtos <potenkov@gmail.com>
2022-04-05 21:14:57 +0400

    Domain model implementation for the academic discipline entity.

---
+++ src/Domain/Model/AcademicDiscipline/AcademicDisciplineRepository.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Edeans\Domain\Model\AcademicDiscipline;
+
+interface AcademicDisciplineRepository
+{
+    public function store(AcademicDiscipline $discipline): void;
+
+    public function remove(AcademicDisciplineId $id): void;
+}
