3d66438de2682558b87ef390f923a0a930f442c4
vtos <potenkov@gmail.com>
2022-04-05 21:55:33 +0400

    Domain model implementation for the curriculum discipline entity.

---
+++ src/Domain/Model/CurriculumDiscipline/CurriculumDiscipline.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Edeans\Domain\Model\CurriculumDiscipline;
+
+use Edeans\Domain\Model\AcademicDiscipline\AcademicDiscipline;
+use Edeans\Domain\Model\FormOfControl\FormOfControl;
+use Edeans\Domain\Model\Term\Term;
+
+class CurriculumDiscipline
+{
+    private CurriculumDisciplineId $id;
+
+    private AcademicDiscipline $academicDiscipline;
+
+    private Term $term;
+
+    private FormOfControl $formOfControl;
+
+    public function __construct(
+        CurriculumDisciplineId $id,
+        AcademicDiscipline $academicDiscipline,
+        Term $term,
+        FormOfControl $formOfControl
+    ) {
+        $this->id = $id;
+        $this->academicDiscipline = $academicDiscipline;
+        $this->term = $term;
+        $this->formOfControl = $formOfControl;
+    }
+}
---
+++ src/Domain/Model/CurriculumDiscipline/CurriculumDisciplineId.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Edeans\Domain\Model\CurriculumDiscipline;
+
+use Edeans\Domain\Model\Common\AggregateId;
+
+final class CurriculumDisciplineId
+{
+    use AggregateId;
+}
