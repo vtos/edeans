a368506eea1f763bf6bcdfa0639bc747702f85b1
vtos <potenkov@gmail.com>
2022-04-09 23:22:02 +0400

    Term domain: added value objects for statuses.

---
+++ src/Domain/Model/Term/VisibilityStatus.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Edeans\Domain\Model\Term;
+
+final class VisibilityStatus
+{
+    private const VISIBLE = 'visible';
+
+    private const HIDDEN = 'hidden';
+
+    private string $status;
+
+    private function __construct(string $status)
+    {
+        $this->status = $status;
+    }
+
+    public static function visible(): self
+    {
+        return new self(self::VISIBLE);
+    }
+
+    public static function hidden(): self
+    {
+        return new self(self::HIDDEN);
+    }
+}
