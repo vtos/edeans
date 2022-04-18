2231ca725c1cf3f6d94a9426545333b2818643f4
vtos <potenkov@gmail.com>
2022-04-18 21:38:36 +0400

    Updated application use case: add term.

---
+++ src/Application/AddTerm/AddTerm.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Edeans\Application\AddTerm;
+
+final class AddTerm
+{
+    public string $name;
+}
--- src/Application/Application.php
+++ src/Application/Application.php
@@ -4,14 +4,15 @@ declare(strict_types=1);
 
 namespace Edeans\Application;
 
+use Edeans\Application\AddTerm\AddTerm;
 use Edeans\Application\ListTerms\TermsList;
 use Edeans\Domain\Model\Term\TermId;
 
 interface Application
 {
-    public function addTerm(): TermId;
+    public function addTerm(AddTerm $addTerm): TermId;
 
-    public function hideTerm(): void;
+    public function hideTerm(TermId $id): void;
 
     public function listTerms(): TermsList;
 }
